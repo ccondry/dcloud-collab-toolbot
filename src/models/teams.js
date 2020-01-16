0f3612798436ad42311cfe00880d26d7d1aed560
Coty Condry <ccondry@cisco.com>
2020-01-16 13:32:59 -0600

    add delete session function, reply to user

--- src/models/teams.js
+++ src/models/teams.js
@@ -12,35 +12,76 @@ module.exports = {
 async function handleWebhook (body) {
   console.log('collab-toolbot - handleWebhook:', body)
   // ignore messages that we sent
-  // if (body.actorId === body.createdBy) {
-  //   console.log('Webex Teams message from self. ignoring.')
-  //   return
-  // }
+  if (body.actorId === body.createdBy) {
+    console.log('Webex Teams message from self. ignoring.')
+    return
+  }
 
   const roomType = body.data.roomType
-  if (roomType === 'direct') {
-    // direct message - go retrieve the message details
-    const messageId = body.data.id
-    const options = {
-      headers: {
-        'Authorization': `Bearer ${app.token}`
-      }
+  // go retrieve the message details
+  const messageId = body.data.id
+  const options = {
+    headers: {
+      'Authorization': `Bearer ${process.env.WEBEX_BOT_TOKEN}`
     }
-    try {
-      const response = await request.get(`https://api.ciscospark.com/v1/messages/${messageId}`, options)
-      console.log('response.data', response.data)
-      await handleMessage(response.data)
-    } catch (e) {
-      console.error('error during Webex Teams handleWebhook', e.message)
-    }
-  } else {
-    console.log(`Webex Teams webhook received, but it was not direct room type. room type = ${roomType}`)
+  }
+  try {
+    const response = await request.get(`https://api.ciscospark.com/v1/messages/${messageId}`, options)
+    console.log('teams message response.data:', response.data)
+    await handleMessage(response.data)
+  } catch (e) {
+    console.error('error during Webex Teams handleWebhook', e.message)
   }
 }
 
-async function handleMessage ({text, personEmail, personId, roomId, files}) {
+async function handleMessage (roomType, {
+  text,
+  personEmail,
+  personId,
+  roomId,
+  files,
+}) {
   console.log(`collab-toolbot message received from Webex Teams user ${personEmail}:`, text)
 
+  // check for command words
+  // break message into words
+  const words = text.split(' ')
+  if (words[0] === '/session') {
+    // session commands
+    if (words[1] === 'delete' || words[1] === 'remove') {
+      const datacenter = words[2]
+      const id = words[3]
+      console.log(`collab-toolbot received command from ${personEmail} to delete dCloud session info for ${datacenter} ${id}`)
+      // set up mongo query
+      const query = {
+        id,
+        datacenter
+      }
+      let message
+      try {
+        // remove from cloud mongo
+        const results = await db.removeOne('dcloud', 'session', query)
+        if (results.deletedCount === 1) {
+          // success
+          console.log(`collab-toolbot found and deleted dCloud session info for '${datacenter} ${id}'.`)
+          // respond in Teams
+          message = `Successfully deleted dCloud session info for **${datacenter}** **${id}.`
+        } else {
+          // didn't find matching session
+          console.log(`collab-toolbot didn't find a dCloud session matching '${datacenter} ${id}' to delete.`)
+        }
+      } catch (e) {
+        // failed db connection?
+        console.log(`collab-toolbot failed database query to delete dCloud session '${datacenter} ${id}'.`)
+      }
+      // send reply message
+      try {
+        await sendMessage({roomId, toPersonEmail: personEmail, roomType, text: message})
+      } catch (e) {
+        console.log('failed to send reply message to Teams:', e.message)
+      }
+    }
+  }
   // were there any attachments?
   if (files && files.length) {
     console.log(`Webex Teams webhook had file attachments - but I'm not prepared to handle those yet.`)
@@ -68,26 +109,39 @@ async function handleMessage ({text, personEmail, personId, roomId, files}) {
 }
 
 // Get the sender info from FB
-function getSenderInfo(personId, token) {
+async function getSenderInfo(personId) {
   const options = {
     headers: {
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${process.env.WEBEX_BOT_TOKEN}`
     }
   }
-  return axios.get(`https://api.ciscospark.com/v1/people/${personId}`, options)
+  return request.get(`https://api.ciscospark.com/v1/people/${personId}`, options)
 }
 
 // send facebook message from page to user
-async function sendMessage(toPersonEmail, text, {token}) {
+async function sendMessage({toPersonEmail, roomId, text, roomType}) {
   if (!text || text.length === 0) {
     return console.log(`Not sending empty string to Webex Teams.`)
   }
   const url = `https://api.ciscospark.com/v1/messages`
-  const body = {toPersonEmail, text}
+  const body = {
+    text
+  }
+  // if it was a group
+  if (roomType === 'group') {
+    body.roomId = roomId
+  } else {
+    // direct message
+    body.toPersonEmail = toPersonEmail
+  }
   const options = {
     headers: {
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${process.env.WEBEX_BOT_TOKEN}`
     }
   }
-  await request.post(url, body, options)
+  try {
+    await request.post(url, body, options)
+  } catch (e) {
+    throw e
+  }
 }
